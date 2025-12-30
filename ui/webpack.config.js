c47e0b1497c6ec4fe54e4e5b5dac6bcd181645ff
Carter McKinnon <cartermckinnon@gmail.com>
2025-12-29 18:44:58 -0800

    fix: ingress, favicon

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -30,6 +31,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "src/index.html",
       inject: 'body',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/favicon.svg", to: "favicon.svg" }
+      ]
     })
   ],
   output: {
